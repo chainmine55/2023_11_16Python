e52dd1bc80f681f269e76a40abc432a1e3c091f8
chainmine55 <150896057+chainmine55@users.noreply.github.com>
2023-12-19 13:39:05 +0000

    modify

--- lesson11_2/outter/tools.py
+++ lesson11_2/outter/tools.py
@@ -1,27 +1,6 @@
-import random,csv
-def getStudents(students_nums:int=1,scores_nums:int=2) ->list[list]:
-    '''
-    參數:student_nums -> 學生人數\n
-    參數:scores_nums -> 科目數\n
-    '''
-    with open('names.txt',mode='r',encoding='utf-8') as file:
-        names:str = file.read()
+import csv
 
-    nameList:list[str] = names.split('\n')
-
-    students:list[list] = []   #建立二維list
-
-    names:list[str] = random.choices(nameList,k=students_nums)
-    for name in names:
-        stu:list[int|str]=[]
-        stu.append(name)
-        for i in range(scores_nums):
-            stu.append(random.randint(40,100))
-        students.append(stu)
-
-    return students
-
-def saveToCSV(fileName:str,date:list[list],subject_nums:int)->None:
+def saveToCSV(fileName:str,date:list[list],subject_nums:int) -> bool:
     fileName +=".csv"
     subjects = [f'科目{+1}'for i in range(subject_nums)]
     fields = ['姓名']
