e5a947deef61310ae471b6350e95decc0d1d026b
chainmine55 <150896057+chainmine55@users.noreply.github.com>
2024-01-30 12:01:57 +0000

    modify

--- line_talk/main.py
+++ line_talk/main.py
@@ -6,5 +6,7 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-if __name__=="__main__":
-    app.run(host='0.0.0.0',port=5000)
+#使用gunicorn main:app,並不會執行以下的程式
+#if __name__ == "__main__":
+#    print("Hello! World!")
+#    app.run(host='0.0.0.0',port=5000)
