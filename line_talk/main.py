f8f1297d57322a01aa47c1d76d6a20286cf837ee
chainmine55 <150896057+chainmine55@users.noreply.github.com>
2024-01-30 13:28:56 +0000

    modify

--- line_talk/main.py
+++ line_talk/main.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
+import os
 
 app = Flask(__name__)
 
+line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
+handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
+
 
 #使用gunicorn main:app,並不會執行以下的程式
 #if __name__ == "__main__":
